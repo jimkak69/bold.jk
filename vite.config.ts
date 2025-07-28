934c5ae302b6929974e7baac2d81589818ec9cf9
jimkak69 <bussines.flyingrc@gmail.com>
2025-07-28 17:16:37 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
+    base: '/bold.jk/',  // <--- Replace this with your GitHub repo name
     const env = loadEnv(mode, '.', '');
     return {
       define: {
